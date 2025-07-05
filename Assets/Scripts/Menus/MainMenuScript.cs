bada456080ebe97a747a6eb34e2412c024f11bcb
Asaf <asaflumer@gmail.com>
2025-07-05 23:42:39 +0300

    Fixed superslash arrow and transition to scene 2

--- Assets/Scripts/Menus/MainMenuScript.cs
+++ Assets/Scripts/Menus/MainMenuScript.cs
@@ -1,12 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
-    public static void PlayGame() {
-        SceneManager.LoadScene("Forest 2");
+    public static void PlayGame()
+    {
+        SceneManager.LoadScene("Forest");
         Time.timeScale = 1;
     }
 
