d2760283121cfa3d6c35b8492f0452096b3e2278
RonyL02 <280202rony@gmail.com>
2025-07-10 00:54:50 +0300

    add raycast to slimes

--- Assets/Scripts/Enemies/SlimeMovement.cs
+++ Assets/Scripts/Enemies/SlimeMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,7 @@ using UnityEngine;
 public class SlimeMovement : MonoBehaviour
 {
     private Transform player;
-    public float maxViewingDistance;
+    private float maxViewingDistance = 10f;
     public Rigidbody2D rb;
     public float jumpHeight;
     public float jumpWidth;
@@ -18,22 +19,30 @@ public class SlimeMovement : MonoBehaviour
 
     void Update()
     {
-        if (player == null) return; 
-        Vector2 moveDir = new Vector2(player.position.x - transform.position.x, player.position.y - transform.position.y);
-        if (Mathf.Abs(moveDir.magnitude) < maxViewingDistance && !isJumping) {
-            StartCoroutine(MoveToPlayer(moveDir.normalized));
+        if (player == null) return;
+
+        Vector2 moveDir = player.position - transform.position;
+        bool isInRange = Math.Abs(moveDir.magnitude) < maxViewingDistance;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, moveDir.normalized, maxViewingDistance, LayerMask.GetMask("Ground"));
+        if (isInRange && !hit)
+        {
+            if (!isJumping)
+            {
+                StartCoroutine(MoveToPlayer(moveDir.normalized));
+            }
         }
     }
 
-    IEnumerator MoveToPlayer(Vector2 moveDir) {
+    IEnumerator MoveToPlayer(Vector2 moveDir)
+    {
         isJumping = true;
         rb.AddForce(new Vector2(moveDir.x * jumpWidth, jumpHeight), ForceMode2D.Impulse);
         yield return new WaitForSeconds(1f);
         isJumping = false;
     }
 
-	private void OnDrawGizmosSelected()
-	{
+    private void OnDrawGizmosSelected()
+    {
         UnityEditor.Handles.DrawWireDisc(transform.position, Vector3.forward, maxViewingDistance);
     }
 }
