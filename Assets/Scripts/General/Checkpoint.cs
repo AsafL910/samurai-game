5f4a205047e2d6a0da38da65ad3281f737d51981
RonyL02 <280202rony@gmail.com>
2025-07-25 15:49:38 +0300

    add checkpoints

--- Assets/Scripts/General/Checkpoint.cs
+++ Assets/Scripts/General/Checkpoint.cs
@@ -6,9 +6,9 @@ public class Checkpoint : MonoBehaviour
 {
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
-		if (collision.CompareTag("Player")) 
+		if (collision.CompareTag("Player"))
 		{
-			FindObjectsOfType<GameManager>()[0].checkpoint = transform.position;
+			GameManager.instance.checkpoint = transform.position;
 		}
 	}
 }
